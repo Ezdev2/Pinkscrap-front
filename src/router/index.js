935ef3fc18dfdade9becd5838ac6cec1816c7dd1
ezdev2 <fansoaezra@gmail.com>
2024-03-22 16:48:33 +0300

    fix routes

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory('/'),
     // history: createWebHistory(""),
     routes,
     scrollBehavior: (to) => {
