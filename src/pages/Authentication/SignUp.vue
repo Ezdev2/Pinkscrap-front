7d438534c5f6941a43df1e62bbb2b2a2f807205b
davdev24 <156902177+davdev24@users.noreply.github.com>
2024-03-29 16:24:56 +0100

    Handle userInfo for registration and subscription

--- src/pages/Authentication/SignUp.vue
+++ src/pages/Authentication/SignUp.vue
@@ -1,6 +1,12 @@
 <script setup>
+import Button from "@/components/Common/Button.vue";
 import image from "../../assets/details-2.png";
 import { useRouter } from "vue-router";
+
+//Ajout ref de vue et de HomeService
+import { ref, watch } from 'vue';
+import HomeService from "../../service/homeService";
+
 const router = useRouter();
 const HomeBtn = () => {
   router.push("/");
@@ -8,6 +14,136 @@ const HomeBtn = () => {
 const Login = () => {
   router.push("/login");
 };
+
+//Ajout des infos du client
+const firstname = ref('');
+const name = ref('');
+const email = ref('');
+const phoneNumber = ref('');
+const enterprise = ref('');
+const comment = ref('');
+const password = ref('');
+const confirmPassword = ref('');
+
+//Gestion d'erreur pour chaque champs 
+const showMissingFirstnameFieldErrorMessage = ref(false);
+const showMissingNameFieldErrorMessage = ref(false);
+const showEmailErrorMessage = ref(false);
+const showPhoneNumberErrorMessage = ref(false);
+const showPasswordErrorMessage = ref(false);
+const showMatchErrorMessage = ref(false);
+
+//Test email
+function isValidEmail(email) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let isValid = emailPattern.test(email);
+    return isValid;
+}
+
+//Test numéro téléphone
+function isValidPhoneNumber(phoneNumber) {
+    const phonePattern = /^\d+$/;
+    let isValid = phonePattern.test(phoneNumber);
+    return isValid;
+}
+
+// Test mdp
+function isPasswordValid(password) {
+    //au moins 1 lettre en minuscule et en majuscule, 1 chiffre et 8 caractères
+    const passwordPattern = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
+    let isValid = passwordPattern.test(password);
+    return isValid ? true : false;
+}
+
+// Test correspondance de mdp
+function passwordsMatch(password, confirmPassword) {
+    return password === confirmPassword;
+}
+
+watch(
+  email, 
+  (newEmail) => {
+    showEmailErrorMessage.value = !isValidEmail(newEmail);
+});
+
+watch(
+  phoneNumber, 
+  (newPhoneNumber) => {
+    showPhoneNumberErrorMessage.value = !isValidPhoneNumber(newPhoneNumber);
+});
+
+//Affichage d'erreur lors de la soumission du formulaire d'inscription
+const displayError = (error) => {
+  const consoleContainer = document.getElementById('consoleError');
+  consoleContainer.innerHTML = '';
+  const errorMessageElement = document.createElement('p');
+  errorMessageElement.textContent = error;
+  errorMessageElement.style.padding = '10px';
+  errorMessageElement.style.color = 'red';
+  errorMessageElement.style.marginBottom = '5px';
+
+  consoleContainer.appendChild(errorMessageElement);
+  setTimeout(() => {
+    consoleContainer.removeChild(errorMessageElement);
+  }, 8000);
+};
+
+//Ajout de fonction de soumission post du formulaire
+const submitForm  = async () => {
+  try {
+    //Validation de chaque champs utile pour faciliter le traitement serveur 
+    if(!firstname.value.trim()){
+      document.getElementById("firstname").focus();
+      showMissingFirstnameFieldErrorMessage.value = true;
+        setTimeout(() => {
+          showMissingFirstnameFieldErrorMessage.value = false;
+        }, 5000);
+    } else if(!name.value.trim()){
+      document.getElementById("name").focus();
+      showMissingNameFieldErrorMessage.value = true;
+        setTimeout(() => {
+          showMissingNameFieldErrorMessage.value = false;
+        }, 5000);
+    } else if(isValidEmail(email.value) && isValidPhoneNumber(phoneNumber.value)){
+      if(!isPasswordValid(password.value)){
+        showPasswordErrorMessage.value = true;
+        setTimeout(() => {
+            showPasswordErrorMessage.value = false;
+        }, 5000);
+      } else if(!passwordsMatch(password.value, confirmPassword.value)){
+        showMatchErrorMessage.value = true;
+        setTimeout(() => {
+          showMatchErrorMessage.value = false;
+        }, 5000);
+      } else {
+        const formData = {
+          firstname: firstname.value,
+          name: name.value,
+          email: email.value,
+          phoneNumber: phoneNumber.value,
+          enterprise: enterprise.value,
+          comment: comment.value,
+          password: password.value,
+          confirmPassword: confirmPassword.value,
+        };
+
+        const { errorMessage, status } = await HomeService.postFormData(formData);
+          
+        if (status === 201) {
+          console.log('Formulaire soumis et enregistré avec succès!');
+          Login();
+        } else if (errorMessage) {
+          document.getElementById("firstname").focus();
+          displayError('Erreur lors de l\'inscription : ' + errorMessage);
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Erreur lors de la soumission du formulaire d'inscription :", error);
+    return [];
+  }
+};
+
 </script>
 
 <template>
@@ -21,35 +157,60 @@ const Login = () => {
         <h1>Inscription</h1>
         <div class="d-block">
           <div class="div-inp">
+            <div id="consoleError" style="margin: 0 auto;"/>
             <label>Prenom *</label>
-            <input type="text" placeholder="Prenom" />
+            <!--Ajout de v-model pour le traitement data et affichage d'erreur lors de l'inscription-->
+            <div>
+              <input v-model="firstname" id="firstname" type="text" placeholder="Prenom" autofocus/>
+              <span :style="'color: red; font-size: 14px; margin-top: 5px;'" v-if="showMissingFirstnameFieldErrorMessage">Ce champs est requis.</span>
+            </div>
             <label>Nom *</label>
-            <input type="text" placeholder="Nom" />
+            <div>
+              <input v-model="name" id="name" type="text" placeholder="Nom"/>
+              <span :style="'color: red; font-size: 14px; margin-top: 5px;'" v-if="showMissingNameFieldErrorMessage">Ce champs est requis.</span>
+            </div>
             <label>Adresse email *</label>
-            <input type="email" placeholder="Adresse email" />
+            <div>
+                <input v-model="email" type="email" placeholder="Adresse email"/>
+                <span :style="'color: red; font-size: 14px; margin-top: 5px;'" v-if="showEmailErrorMessage">Email invalide</span>
+            </div>
             <label>Numero Telephone *</label>
-            <input type="tel" placeholder="Numero Telephone" />
+            <div>
+                <input v-model="phoneNumber" type="tel" placeholder="Numéro de téléphone"/>
+                <span :style="'color: red; font-size: 14px; margin-top: 5px;'" v-if="showPhoneNumberErrorMessage">Numéro de téléphone invalide</span>
+            </div>
           </div>
           <div class="div-inp">
-            <label>Entreprise *</label>
-            <input type="text" placeholder="Entreprise" />
-            <label>Comment Allez-vous</label>
-            <select>
+            <label>Entreprise</label>
+            <input v-model="enterprise" type="text" placeholder="Entreprise"/>
+            <label>Comment allez-vous</label>
+            <select v-model="comment">
               <option value="option1">Option 1</option>
               <option value="option2">Option 2</option>
               <option value="option3">Option 3</option>
             </select>
             <label>Mot de passe *</label>
-            <input type="password" placeholder="Mot de passe" />
+            <div>
+                <input v-model="password" type="password" placeholder="Mot de passe"/>
+                <span :style="'color: red; font-size: 14px; margin-top: 5px;'" v-if="showPasswordErrorMessage">Mot de passe invalide! Le mot de passe doit contenir : 1 lettre en minuscule et en majuscule, 1 chiffre et au moins 8 caractères.</span>
+            </div>
             <label>Confirmation *</label>
-            <input type="password" placeholder="Confirmation" />
+            <div>
+                <input v-model="confirmPassword" type="password" placeholder="Confirmation"/>
+                <span :style="'color: red; font-size: 14px; margin-top: 5px;'" v-if="showMatchErrorMessage">Les mots de passe ne correspondent pas.</span>
+            </div>
           </div>
         </div>
         <div class="division">
           <p class="span-compte">
             Vous avez déjà un compte? <a @click="Login">Connexion</a>
           </p>
-          <button>S'inscrire</button>
+          <Button
+            style="width: fit-content"
+            type="primary"
+            :label="'S\'inscrire'"
+            @click="submitForm"
+          />
         </div>
       </div>
     </div>
@@ -212,5 +373,6 @@ button {
 .color{
   height: 100vh;
 }
+
 }
 </style>
