ed825e98301a7026d2e06b794fc056a884b712cd
davdev24 <156902177+davdev24@users.noreply.github.com>
2024-03-29 16:23:23 +0100

    handle cookie token, login and stripe subscription for new or current customers

--- src/pages/Authentication/Login.vue
+++ src/pages/Authentication/Login.vue
@@ -1,6 +1,10 @@
 <script setup>
+import Button from "@/components/Common/Button.vue";
 import image from "../../assets/details-2.png";
 import { useRouter } from "vue-router";
+import { ref, onMounted } from 'vue';
+import HomeService from "../../service/homeService";
+
 const router = useRouter();
 const HomeBtn = () => {
   router.push("/");
@@ -8,6 +12,99 @@ const HomeBtn = () => {
 const SignUp = () => {
   router.push("/register");
 };
+
+let token = null;
+
+onMounted(() => {
+    //Ajout de récupération de token par cookie
+    token = getCookie('token');
+});
+
+const email = ref('');
+const password = ref('');
+const showEmailErrorMessage = ref(false);
+const showPasswordErrorMessage = ref(false);
+
+function getCookie(id) {
+   let value = document.cookie.match('(^|;)?' + id + '=([^;]*)(;|$)');
+   return value ? unescape(value[2]) : null;
+}
+
+//Affichage d'erreur de connexion
+const displayError = (error) => {
+  const consoleContainer = document.getElementById('consoleError');
+  consoleContainer.innerHTML = '';
+  const errorMessageElement = document.createElement('p');
+  errorMessageElement.textContent = error;
+  errorMessageElement.style.padding = '10px';
+  errorMessageElement.style.color = 'red';
+  errorMessageElement.style.marginBottom = '5px';
+
+  consoleContainer.appendChild(errorMessageElement);
+  setTimeout(() => {
+    consoleContainer.removeChild(errorMessageElement);
+  }, 8000);
+};
+
+function setCookie(id, value) {
+  // Définit un cookie expirant dans 7 jours
+  // var date = new Date();
+  // date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+  // var expires = ";expires=" + date.toUTCString();
+  document.cookie = id + '=' + value;
+}
+
+const submitLoginData  = async () => {
+  try {
+    if(!email.value.trim()) {
+      document.getElementById("email").focus();
+      showEmailErrorMessage.value = true;
+        setTimeout(() => {
+          showEmailErrorMessage.value = false;
+        }, 3000);
+    } else if(!password.value.trim()){
+      showPasswordErrorMessage.value = true;
+        setTimeout(() => {
+          showPasswordErrorMessage.value = false;
+        }, 3000);
+    } else {
+        const loginData = {
+          email: email.value,
+          password: password.value,
+        };
+
+        const { errorMessage, data, status } = await HomeService.postLoginData(loginData);
+        
+        if (status === 200) {
+          const { error, customerUrl, url } = await HomeService.getSubscription({
+            email: data.user.email,
+            id: data.user.id,
+          }, data.token);
+          if (customerUrl) {
+            console.log('Client authentifié.');
+            window.location.replace(customerUrl);
+            setCookie('token', data.token);
+          } else if (url) {
+            console.log('Nouveau client authentifié.');
+            window.location.replace(url);
+          } else if (error) {
+              console.log('Erreur d\'authentification Stripe :', error);
+          } else {
+            displayError('Erreur d\'authentification.');
+          }
+        } else {
+          console.error(errorMessage);
+          displayError('Erreur de connexion : ' + errorMessage);
+        }
+    }         
+    
+  } catch (error) {
+    console.error("Erreur lors de la soumission des données de connexion :", error);
+    return [];
+  }
+
+};
+
 </script>
 
 <template>
@@ -19,17 +116,29 @@ const SignUp = () => {
       </a>
       <div class="color">
         <div class="division">
-          <h1>Inscription</h1>
+          <h1>Connexion</h1>
+          <!--Ajout de console d'erreur lors du traitement de la connexion-->
+          <div id="consoleError" style="margin: 0 auto;"/>
           <div class="d-block">
             <label>Adresse email *</label>
-            <input type="email" placeholder="Adresse email" />
+            <!--Ajout de span d'erreur propre à chaque champs-->
+            <span :style="'color: red; font-size: 14px; margin-top: 5px;'" v-if="showEmailErrorMessage">Votre email est requis.</span>
+            <input v-model="email" id="email" type="email" placeholder="Adresse email"/>
+            
             <label>Mot de passe *</label>
-            <input type="password" placeholder="Mot de passe" />
+            <span :style="'color: red; font-size: 14px; margin-top: 5px;'" v-if="showPasswordErrorMessage">Mot de passe requis.</span>
+            <input v-model="password" id="password" type="password" placeholder="Mot de passe"/>
           </div>
           <p class="span-compte">
             Vous n'avez pas un compte? <a @click="SignUp">Inscrivez-vous ici</a>
           </p>
-          <button>Se connecter</button>
+          <!--Import de Button.vue et écoute du bouton de connexion-->
+          <Button
+            style="width: fit-content"
+            type="primary"
+            :label="'Se connecter'"
+            @click="submitLoginData"
+          />
         </div>
       </div>
     </div>
